c048d7c33d981f5c53f75f45663e0ac63aa64b24
PTVNhan <ptvncntt@gmail.com>
2018-04-28 01:09:24 +0700

    Làm them switch..case + - x :

---
+++ app/src/main/java/com/example/nhanphan/tinhtong2so_intents/Chia.java
@@ -0,0 +1,13 @@
+package com.example.nhanphan.tinhtong2so_intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chia);
+    }
+}
---
+++ app/src/main/java/com/example/nhanphan/tinhtong2so_intents/Tru.java
@@ -0,0 +1,13 @@
+package com.example.nhanphan.tinhtong2so_intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tru);
+    }
+}
